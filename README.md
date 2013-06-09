3661cc263b933826fc1b75ea32f9663eb422acf9
Paul Shi <hipaulshi@gmail.com>
2013-06-09 15:15:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Locationbot
+show location of users, it will speak out in the channel
+
+#Setup
+```npm install```
+change your config in server.js to your irc server and channel. you can specify the bot name there as well
+
+#Run
+```node server.js```
+
+the command to manually query the location in the channel is by using
+
+```showlocation```
+
+#Known Bugs
+Memory leak from listener.
---
+++ server.js
@@ -0,0 +1,76 @@
+// Create the configuration
+var config = {
+    channels: ["#achannel you want to join in"], //specify the channel you want to join in here
+    server: "irc.freenode.net",
+    botName: "locationbot"
+};
+
+// Get the lib
+var irc = require("irc")
+  ,	request = require("request")
+  , http = require("http");
+
+// Create the bot name
+var bot = new irc.Client(config.server, config.botName, {
+    channels: config.channels
+});
+
+Object.prototype.keys = function ()
+{
+  var keys = [];
+  for(var i in this) if (this.hasOwnProperty(i))
+  {
+    keys.push(i);
+  }
+  return keys;
+}
+
+// Listen for any message, say to him/her in the room
+bot.addListener('message', function (from, to, message) {
+	if (message == "showlocation")
+	{
+	//	bot.say(config.channels[0],"issue names command");
+    	bot.send('NAMES', config.channels[0]);
+    }
+
+});
+
+bot.addListener("names",function (channel, nicks) { 
+	var keys = [];
+	var printstringcontent=[];
+	for(var i in nicks) if (nicks.hasOwnProperty(i))
+	{
+		keys.push(i);
+		bot.whois(i,function(doc){
+			//console.log("nick: ",doc.nick);
+			var request_addr="http://ip-api.com/json/"+doc.host;
+			//console.log("requst addr:", request_addr);
+			request(request_addr, function (error, response, body) {
+  				if (!error && response.statusCode == 200) {
+	    			//console.log(body) // Print the google web page.
+	    			var body=JSON.parse(body);
+					if (("city" in body) && ("region" in body))//both must exist
+					{
+						var contentstring="["+doc.nick+"-"+body.city+","+body.region+"]";
+					}
+					else
+					{
+						var contentstring="["+doc.nick+"-"+"N/A"+"]";
+					}
+					printstringcontent.push(contentstring);
+					// console.log(printstringcontent.length);
+					// console.log(Object.keys(nicks).length);
+					if (printstringcontent.length==Object.keys(nicks).length)
+					{
+						console.log(printstringcontent);
+						bot.say(config.channels[0],printstringcontent);
+					}
+  				}
+			});
+
+
+		});
+	}
+	//console.log(keys);;
+
+})
